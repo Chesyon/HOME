ae04dff0fd8134df9bc4499d0186592305ea347a
Chesyon <55322011+Chesyon@users.noreply.github.com>
2024-11-04 08:29:03 -0500

    11/4/24
    mostly portraits

---
+++ Assets/TextAdventure/BoxColor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxColor : MonoBehaviour
+{
+    public Image bg;
+    public Gradient colorRange;
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        bg.color = colorRange.Evaluate((Mathf.Sin(Time.time * speed) + 1) / 2);
+    }
+}
